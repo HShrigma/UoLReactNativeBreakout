22b36b8d3b1da8aff45f21a49aa562dc7ba451e5
Hristo Stantchev <hristostantchev@abv.bg>
2024-02-06 16:36:48 +0200

    App gets user dimensions, styles moved to main to use them

--- BreakerInfinite/App.js
+++ BreakerInfinite/App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useWindowDimensions } from 'react-native';
+
+const title = "Breaker: Infinite";
+
+function UseStyles(){
+  const {width,height} = useWindowDimensions();
+  return StyleSheet.create({
+    Background: {
+      width: width,
+      height: height,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paddle: {
+      width: width/5,
+      height: height/20,
+      backgroundColor: "#fff"
+    }
+  });
+}
 
 export default function App() {
+  //loading stylesheets in so they can make use of width/height dynamic dimensions
+  const styles = UseStyles();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.Background}>
+      <Text style = {"color:#fff"}>{title}</Text>
+      <View style={styles.paddle}>
+
+      </View>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
